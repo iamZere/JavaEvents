5a15fc3ee3a327e23386010c21cf6c13f56d8bdc
David <dpmayxat156@gmail.com>
2025-04-25 00:37:46 +0200

    Clase clientes

--- demo/src/main/java/com/example/Clientes.java
+++ demo/src/main/java/com/example/Clientes.java
@@ -1,5 +1,69 @@
 package com.example;
 
 public class Clientes {
-    
+
+    private String nombre;
+    private String correo;
+    private String clave;
+    private String telefono;
+    private Boolean vip;
+
+    public Clientes(String nombre, String correo, String clave, String telefono, Boolean vip) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.clave = clave;
+        this.telefono = telefono;
+        this.vip = vip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Boolean getVip() {
+        return vip;
+    }
+
+    public void setVip(Boolean vip) {
+        this.vip = vip;
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" +
+                "nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                ", clave='" + clave + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", vip=" + vip +
+                '}';
+        }
 }
