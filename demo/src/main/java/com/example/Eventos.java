af0c11d922af4f2a6e7e3bb212abb9f5cb644295
David <dpmayxat156@gmail.com>
2025-04-25 01:10:53 +0200

    Clase eventos

--- demo/src/main/java/com/example/Eventos.java
+++ demo/src/main/java/com/example/Eventos.java
@@ -1,5 +1,10 @@
 package com.example;
 
 public class Eventos {
-    
+    private String titulo;
+    private String tipo;
+    // fecha y hora del evento, puede celebrarse una vez o repetirse
+    private Integer precio;
+    // portada del evento en formato imagen (para la interfaz)
+    private Integer calificacion; //calificacion del evento (1-5)
 }
