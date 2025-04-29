591fd8d55f005e140eb8d139c08d4a62bb139e96
David <dpmayxat156@gmail.com>
2025-04-29 22:57:51 +0200

    Añadido metodo busqueda de eventos por tipo

--- demo/src/test/java/com/app/JavaEvents.java
+++ demo/src/test/java/com/app/JavaEvents.java
@@ -24,16 +24,22 @@ public class JavaEvents {
     //1º metodo: permite buscar eventos por ciudad. Se espera que la ciudad sea un String y se devuelve una lista de eventos que coinciden con la ciudad proporcionada.
     public ArrayList<Evento> buscarEventosPorCiudad(String ciudad) {
         ArrayList<Evento> eventosEncontrados = new ArrayList<>(); // Lista para almacenar los eventos encontrados
-        for (Evento evento : reservaEventos) { // Itera sobre la lista de eventos
+        for (Evento evento : reservaEventos) { 
             if (evento.getLugar().equalsIgnoreCase(ciudad)) { // Compara el lugar del evento con la ciudad proporcionada
                 eventosEncontrados.add(evento); // Si coinciden, agrega el evento a la lista de eventos encontrados
             }
         }
-        return eventosEncontrados; // Devuelve la lista de eventos encontrados
+        return eventosEncontrados; 
     }
 
     //2º metodo: permite buscar eventos por tipo, siendo Concierto, Deporte, Musical o Teatro las opciones posibles.
-    public void buscarEventosPorTipo(String tipo) {
-        
+    public ArrayList<Evento> buscarEventosPorTipo(String tipo) {
+        ArrayList<Evento> eventosPorTipo = new ArrayList<>(); 
+        for (Evento evento : reservaEventos) { 
+            if (evento.getTipo().equalsIgnoreCase(tipo)) { // Compara el tipo del evento con el tipo proporcionado
+                eventosPorTipo.add(evento); // Si coinciden, agrega el evento a la lista de eventos encontrados
+            }
+        }
+        return eventosPorTipo;
     }
 }
